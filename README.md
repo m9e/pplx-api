204cad0e58c7c97f9bbeba0b5b98d88a8cd21602
Matt Wallace <git@savagetech.com>
2024-10-22 09:06:10 -0600

    v0.1 commit final

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# pplx - Python Client for Perplexity AI API
+# pplx-api - Python Client for Perplexity AI API
 
-[![PyPI version](https://badge.fury.io/py/pplx.svg)](https://badge.fury.io/py/pplx)
+[![PyPI version](https://badge.fury.io/py/pplx-api.svg)](https://badge.fury.io/py/pplx-api)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-`pplx` is a Python client library for interacting with the Perplexity AI API. It provides a simple and intuitive interface to make chat completion requests to Perplexity's advanced language models.
+`pplx-api` is a Python client library for interacting with the Perplexity AI API. It provides a simple and intuitive interface to make chat completion requests to Perplexity's advanced language models.
 
 ## Installation
 
-You can install `pplx` using pip:
+You can install `pplx-api` using pip:
+
+```bash
+pip install pplx-api
+```
+
--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='pplx',
+    name='pplx-api',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Kamiwaza AI',
+    author_email='opensource@kamiwaza.ai',
     description='A Python client for the Perplexity AI API',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/pplx',
+    url='https://github.com/kamiwaza-ai/pplx-api',
     packages=find_packages(exclude=['tests*']),
     install_requires=requirements,
     classifiers=[
@@ -25,18 +25,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.10',
     include_package_data=True,
-    package_data={'': ['LICENSE']},
-    entry_points={
-        'console_scripts': [
-            'pplx=pplx.cli:main',
-        ],
-    },
+    package_data={'pplx-api': ['LICENSE']},
+    py_modules=['pplx-api'],
 )
